3bca1ba47470eab1e5535ce4ca0a96b11c470d85
Bablu Mehta <babloomehta895012@gmail.com>
2023-11-06 20:33:45 +0530

    login bug fixed

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import Signup from './pages/Login/Signup';
+import ProtectedRoute from './pages/ProtectedRoute';
+import PageLoading from './pages/PageLoading';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<Signup/>} />
+        <Route path='/page-loading' element={<PageLoading/>} />
       </Routes>
       </BrowserRouter>
     </div>
