9b0363a577d069a51febe67f9159c3eae1a793db
Bablu Mehta <babloomehta895012@gmail.com>
2023-11-11 08:13:41 +0530

    added tweet section to the feed of the user and successfully connected to backend server of twitter clone

--- src/pages/Feed/TweetBox.js
+++ src/pages/Feed/TweetBox.js
@@ -3,109 +3,51 @@ import "./TweetBox.css";
 import { Avatar, Button } from "@mui/material";
 import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
 import axios from "axios";
-import { useUserAuth } from "../../context/UserAuthContext";
-import useLoggedInUser from "../../hooks/useLoggedInUser";
-
 function TweetBox() {
-    const [post, setPost] = useState('')
-    const [imageURL, setImageURL] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
-    const [name, setName] = useState('');
-    const [username, setUsername] = useState(' ');
-    const [loggedInUser] = useLoggedInUser();
-    const { user } = useUserAuth();
-    const email = user?.email;
+    const [post, setPost] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
 
 
-    const userProfilePic = loggedInUser[0]?.profileImage ? loggedInUser[0]?.profileImage : "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
-
-    // console.log(user?.providerData[0]?.providerId);
-
-    const handleUploadImage = e => {
-        setIsLoading(true);
-        const image = e.target.files[0];
-
-        const formData = new FormData();
-        formData.set('image', image)
-
-        axios.post("https://api.imgbb.com/1/upload?key=c1e87660595242c0175f82bb850d3e15", formData)
-            .then(res => {
-                setImageURL(res.data.data.display_url);
-                // console.log(res.data.data.display_url);
-                setIsLoading(false)
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
-
-    const handleTweet = (e) => {
+    const handleTweet = (e) =>{
         e.preventDefault();
-
-        if (user?.providerData[0]?.providerId === 'password') {
-            fetch(`https://pacific-peak-30751.herokuapp.com/loggedInUser?email=${email}`)
-                .then(res => res.json())
-                .then(data => {
-                    setName(data[0]?.name)
-                    setUsername(data[0]?.username)
-                })
+        
+        const userPost = {
+            post:post,
+            photo:imageUrl
         }
-        else {
-            setName(user?.displayName)
-            setUsername(email?.split('@')[0])
-        }
-
-        if (name) {
-            const userPost = {
-                profilePhoto: userProfilePic,
-                post: post,
-                photo: imageURL,
-                username: username,
-                name: name,
-                email: email,
-            }
-            console.log(userPost);
-            setPost('')
-            setImageURL('')
-            fetch('https://pacific-peak-30751.herokuapp.com/post', {
-                method: "POST",
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify(userPost),
-            })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data);
-
-                })
-        }
+        console.log(userPost);
+        fetch('http://localhost:5000/post',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(userPost)
+        }).then((res)=>res.json()).then(data => console.log(data));
     }
 
     return <div className="tweetBox">
         <form onSubmit={handleTweet}>
             <div className="tweetBox__input">
-                <Avatar src={loggedInUser[0]?.profileImage ? loggedInUser[0]?.profileImage : "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"} />
+                <Avatar src='' />
                 <input
                     type="text"
                     placeholder="What's happening?"
                     onChange={(e) => setPost(e.target.value)}
-                    value={post}
-                    required
+                   
                 />
 
             </div>
             <div className="imageIcon_tweetButton">
                 <label htmlFor='image' className="imageIcon">
-                    {
-                        isLoading ? <p>Uploading Image</p> : <p>{imageURL ? 'Image Uploaded' : <AddPhotoAlternateOutlinedIcon />}</p>
-                    }
+                  
+                         <AddPhotoAlternateOutlinedIcon />
+                    
                 </label>
                 <input
                     type="file"
                     id='image'
                     className="imageInput"
-                    onChange={handleUploadImage}
+                    
                 />
                 <Button className="tweetBox__tweetButton" type="submit">Tweet</Button>
             </div>
