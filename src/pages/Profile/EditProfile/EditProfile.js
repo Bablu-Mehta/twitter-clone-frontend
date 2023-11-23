c856cf7f273ee0fb766c4fbabd6380fb2de098ba
Bablu Mehta <babloomehta895012@gmail.com>
2023-11-23 08:17:43 +0530

    made some progress in Edit Profile section

--- src/pages/Profile/EditProfile/EditProfile.js
+++ src/pages/Profile/EditProfile/EditProfile.js
@@ -1,8 +1,52 @@
 import React from 'react'
 import Modal from '@mui/material/Modal'
 import './EditProfile.css'
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, TextField } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+
+
+const style ={
+  position:'absolute',    
+  top:'50%',    
+  left:'50%',    
+  transform:'translate(-50%, -50%)',    
+  width:'600px',    
+  height:'600px',      
+  bgColor:'black',   
+  boxShadow:24,   
+  borderRadius:8
+}
+
+function EditChild({dob, setDob}){
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () =>{
+    setOpen(true);
+  }
+
+  const handleClose = () =>{
+    setOpen(false);
+  }
+
+  return(
+    <React.Fragment>
+      <div className='birthdate-section' onClick={handleOpen}>
+        <text>Edit</text>
+      </div>
+      <Modal 
+      hideBackdrop
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="child-modal-title"
+      aria-describedby="child-modal-description"
+      >
+        <Box>
+          
+        </Box>
+      </Modal>
+    </React.Fragment>
+  )
+}
 
 export default function EditProfile({user, loggedInUser}) {
   const [open, setOpen] = React.useState(false);
@@ -12,16 +56,11 @@ export default function EditProfile({user, loggedInUser}) {
   const [website, setWebsite] = React.useState('');
   const [dob, setDob] = React.useState('');
 
-  const style ={
-      position:'absolute',    
-      top:'50%',    
-      left:'50%',    
-      transform:'translate(-50%, -50%)',    
-      width:'600px',    
-      height:'600px',      
-      bgColor:'black',   
-      boxShadow:24,   
-      borderRadius:8
+  
+
+  const handleSave = () =>{
+    console.log('saving...');
+    setOpen(false);
   }
   
   return (
@@ -36,9 +75,27 @@ export default function EditProfile({user, loggedInUser}) {
         <Box sx={style} className='modal'>
             <div className='header'>
               <IconButton onClick={() =>{setOpen(false)}}><CloseIcon/></IconButton>
+              <h2 className='header-title'>Edit Profile</h2>
+              <button className='save-btn' onClick={handleSave}>Save</button>
             </div>
-            <div className='fill-content'></div>
-            <div className='birthDate-selection'></div>
+            <form className='fill-content'>
+              <TextField className='text-field' fullWidth label='Name' is='fullWidth' 
+              variant='filled' onChange={(e)=>{setName(e.target.value)}} defaultValue={loggedInUser?.name ? loggedInUser?.name : ''}/> 
+
+              <TextField className='text-field' fullWidth label='Bio' is='fullWidth' 
+              variant='filled' onChange={(e)=>{setBio(e.target.value)}} defaultValue={loggedInUser?.bio ? loggedInUser?.bio : ''}/>
+
+              <TextField className='text-field' fullWidth label='Location' is='fullWidth' 
+              variant='filled' onChange={(e)=>{setLocation(e.target.value)}} defaultValue={loggedInUser?.location ? loggedInUser?.location : ''}/>
+
+              <TextField className='text-field' fullWidth label='Website' is='fullWidth' 
+              variant='filled' onChange={(e)=>{setWebsite(e.target.value)}} defaultValue={loggedInUser?.website ? loggedInUser?.website : ''}/>    
+            </form>
+            <div className='birthDate-selection'>
+              <p>Birth Date</p>
+              <p>.</p>
+              <EditChild dob={dob} setDob={setDob} />
+            </div>
             <div className='last-section'></div>
         </Box>
 
